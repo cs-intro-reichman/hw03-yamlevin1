81470d6eb7452a82815a06b1d32607e54050ae33
Yam Levin <yam.levin@runi.ac.il>
2024-01-05 14:21:36 +0200

    calc2

--- LoanCalc.java
+++ LoanCalc.java
@@ -13,12 +13,9 @@ public class LoanCalc {
      */
 	public static void main(String[] args) {		
 		// Gets the loan data
-		//double loan = Double.parseDouble(args[0]);
-		//double rate = Double.parseDouble(args[1]);
-		//int n = Integer.parseInt(args[2]);
-		double loan = 100000;
-		double rate = 3;
-		int n = 12;
+		double loan = Double.parseDouble(args[0]);
+		double rate = Double.parseDouble(args[1]);
+		int n = Integer.parseInt(args[2]);
 		System.out.println("Loan sum = " + loan + ", interest rate = " + rate + "%, periods = " + n);
 		
 		// Computes the periodical payment using brute force search
